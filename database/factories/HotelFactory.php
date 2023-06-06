018a58106025b665b6dfe7d61c0bddecd93f62c8
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-06 06:17:53 +0100

    added new column for seed test

--- database/factories/HotelFactory.php
+++ database/factories/HotelFactory.php
@@ -27,7 +27,7 @@ class HotelFactory extends Factory
             'phone_number' => fake()->phoneNumber(),
             'description' => fake()->text(),
             'rating' => fake()->randomFloat(1, 0, 5),
-            'image' => fake()->randomElement(['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg']),
+            'image' => fake()->randomElement(['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png']),
             'slug' => str_replace(' ', '_', $name)
         ];
     }
