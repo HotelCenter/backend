67386b8a4343e5476cfea7cebc54f0d4ae295e16
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-06 02:50:06 +0100

    A new controller method that filtering hotels by availability,children count,adult count, destination

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hotel;
+use App\Models\Room;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class HotelController extends Controller
 {
@@ -14,10 +17,38 @@ class HotelController extends Controller
      */
     public function index()
     {
-        $hotels=Hotel::orderByDesc('rating')->take(10)->get();
+        $hotels = Hotel::orderByDesc('rating')->take(10)->get();
         return response()->json($hotels);
     }
-
+    public function getHotelsByFilters(Request $request)
+    {
+        $validator = validator::make($request->all(), [
+            "children" => "integer|min:0|required",
+            "rooms" => "integer|min:1|required",
+            "adults" => "integer|min:1|required",
+            "destination" => "string|required"
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $validated = $validator->validated();
+        $destination = $validated['destination'];
+        $rooms_count = $validated['rooms'];
+        $children = $validated['children'];
+        $adults = $validated['adults'];
+        $hotels_query = Hotel::where('city', 'like', '%' . $destination . '%')->
+            orWhere('country', 'like', '%' . $destination . '%')->get();
+        $hotels = [];
+        $rooms = [];
+        foreach ($hotels_query as $hotel) {
+            $filtered_rooms = $hotel->rooms()->where('is_available', true)->where('minimum_children', '>=', $children)->where('minimum_adults', '>=', $adults);
+            if ($filtered_rooms->count() >= $rooms_count) {
+                array_push($hotels, $hotel);
+                array_push($rooms, $filtered_rooms->get());
+            }
+        }
+        return response()->json(['hotels' => $hotels, 'rooms' => $rooms]);
+    }
     /**
      * Show the form for creating a new resource.
      *
@@ -47,7 +78,7 @@ class HotelController extends Controller
      */
     public function show(Hotel $hotel)
     {
-        
+
         return response()->json($hotel);
     }
 
