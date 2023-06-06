fbf7cc6b847a34ba902655d8229d5d1ee2eba40a
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-06 18:27:25 +0100

    added relationship factory between rooms and hotels 5rooms/hotel

--- database/factories/HotelFactory.php
+++ database/factories/HotelFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Hotel>
@@ -16,9 +18,10 @@ class HotelFactory extends Factory
      */
     public function definition()
     {
-        $name = fake()->unique()->name();
+        $name = fake()->unique()->sentence();
+        $slug = Str::slug($name);
         return [
-            'user_id' => fake()->numberBetween(1, 10),
+            'user_id' => User::factory(),
             'name' => $name,
             'address' => fake()->address(),
             'city' => fake()->city(),
@@ -28,7 +31,7 @@ class HotelFactory extends Factory
             'description' => fake()->text(),
             'rating' => fake()->randomFloat(1, 0, 5),
             'image' => fake()->randomElement(['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png']),
-            'slug' => str_replace(' ', '_', $name)
+            'slug' => $slug
         ];
     }
 }
