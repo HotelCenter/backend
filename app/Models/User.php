75342b10942d6dae64d6fffca77cfdabd1a7d96c
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-10 23:07:05 +0100

    paddle not longer needed

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
-use Laravel\Paddle\Billable;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable, Billable;
+    use HasFactory, Notifiable;
     /**
      * The attributes that are mass assignable.
      *
