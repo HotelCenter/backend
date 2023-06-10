57166dda12f011e8dd5b2fdd27e80920f6ec853a
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-10 22:56:02 +0100

    Model Policy for authorziation

---
+++ app/Policies/HotelPolicy.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Hotel;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Http\Request;
+
+class HotelPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny()
+    {
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Hotel  $hotel
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Hotel $hotel)
+    {
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Hotel  $hotel
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Hotel $hotel)
+    {
+        return ($user->is_admin || $user->id == $hotel->user_id) ? Response::allow() : Response::denyWithStatus(403);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Hotel  $hotel
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Hotel $hotel)
+    {
+        return ($user->is_admin || $user->id == $hotel->user_id) ? Response::allow() : Response::denyWithStatus(403);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Hotel  $hotel
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Hotel $hotel)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Hotel  $hotel
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Hotel $hotel)
+    {
+        return ($user->is_admin || $user->id == $hotel->user_id) ? Response::allow() : Response::denyWithStatus(403);
+    }
+}
