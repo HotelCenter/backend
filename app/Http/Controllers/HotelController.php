8516b92055a27ff025b345dd176c227822b745b8
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-10 22:53:44 +0100

    Hotel Resource api, update,index,delete,show

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -3,23 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hotel;
-use App\Models\Room;
-use Illuminate\Database\Query\Builder;
+use App\Models\PaginateModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class HotelController extends Controller
 {
+    public function __construct()
+    {
+
+        $this->authorizeResource(Hotel::class, 'hotel', ['except' => ['show', 'index']]);
+        $this->middleware('auth:api', ['except' => ['show', 'index', 'getRoomsByHotel', 'getHotelsByFilters']]);
+
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $hotels = Hotel::orderByDesc('rating')->take(10)->get();
-        sleep(5);
-        return response()->json($hotels);
+        $response = PaginateModel::paginateAPI(
+            $request,
+            Hotel::count(),
+            Hotel::orderByDesc('rating')->orderBy('created_at')
+        );
+        return response()->json($response);
     }
     public function getHotelsByFilters(Request $request)
     {
@@ -33,7 +43,7 @@ class HotelController extends Controller
 
         ]);
         if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
+            return response()->json($validator->errors(), 422);
         }
         $validated = $validator->validated();
         $destination = $validated['destination'];
@@ -53,15 +63,6 @@ class HotelController extends Controller
         sleep(5);
         return response()->json($hotels);
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -71,7 +72,43 @@ class HotelController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'user_id' => 'required|integer',
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'postcode' => 'required|string|max:10',
+            'country' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:20',
+            'description' => 'required|string',
+            'image' => 'required|string|max:255',
+            'rating' => 'required|numeric|min:0|max:5',
+        ];
+
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $validatedData = $validator->validated();
+        $validatedData['slug'] = Str::slug($validatedData['name']);
+        $newhotel = new Hotel;
+        $newhotel->user_id = $validatedData['user_id'];
+        $newhotel->name = $validatedData['name'];
+        $newhotel->address = $validatedData['address'];
+        $newhotel->city = $validatedData['city'];
+        $newhotel->postcode = $validatedData['postcode'];
+        $newhotel->country = $validatedData['country'];
+        $newhotel->phone_number = $validatedData['phone_number'];
+        $newhotel->description = $validatedData['description'];
+        $newhotel->slug = $validatedData['slug'];
+        $newhotel->image = $validatedData['image'];
+        $newhotel->rating = $validatedData['rating'];
+        $newhotel->save();
+
+
+        return response()->json(["data" => $newhotel]);
     }
 
     /**
@@ -90,16 +127,7 @@ class HotelController extends Controller
         $rooms = $hotel->rooms()->orderBy("date_available")->get();
         return response()->json($rooms);
     }
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Hotel  $hotel
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Hotel $hotel)
-    {
-        //
-    }
+
 
     /**
      * Update the specified resource in storage.
@@ -110,7 +138,36 @@ class HotelController extends Controller
      */
     public function update(Request $request, Hotel $hotel)
     {
-        //
+
+        $rules = [
+            'address' => 'string|max:255',
+            'city' => 'string|max:255',
+            'postcode' => 'string|max:10',
+            'country' => 'string|max:255',
+            'phone_number' => 'string|max:20',
+            'description' => 'string',
+            'image' => 'string|max:255',
+            'rating' => 'numeric|min:0|max:5',
+        ];
+
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $hotel->address = $request->input('name', $hotel->address);
+        $hotel->city = $request->input('city', $hotel->city);
+        $hotel->postcode = $request->input('postcode', $hotel->name);
+        $hotel->country = $request->input('country', $hotel->country);
+        $hotel->phone_number = $request->input('phone_number', $hotel->phone_number);
+        $hotel->description = $request->input('name', $hotel->description);
+        $hotel->image = $request->input('image', $hotel->image);
+        $hotel->rating = $request->input('rating', $hotel->rating);
+        $hotel->save();
+
+
+        return response()->json(["data" => $hotel]);
     }
 
     /**
@@ -121,6 +178,7 @@ class HotelController extends Controller
      */
     public function destroy(Hotel $hotel)
     {
-        //
+        $hotel->delete();
+        return response()->json(["data" => ['message' => 'Hotel deleted successfully']]);
     }
 }
